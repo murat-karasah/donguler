4c581b7f71a1f06b9c1a5bead34999a4da006c87
Murat Karasah <mkarasah@gmail.com>
2021-12-01 11:21:33 +0300

    tersine

--- donguler/Program.cs
+++ donguler/Program.cs
@@ -6,20 +6,22 @@ namespace donguler
     {
         static void Main(string[] args)
         {
-             int sayi=0;
-            int i;
-            for ( i = 1; i < 101; i++)
-            {
-
-                if (i%2==0)
+            /*     int sayi=0;
+                int i;
+                for ( i = 1; i < 101; i++)
                 {
-                     sayi = sayi + i;
+
+                    if (i%2==0)
+                    {
+                         sayi = sayi + i;
+                    }
                 }
+                Console.WriteLine($"{sayi}");*/
 
+            for (int i = 100; i >= 0; i-=4)
+            {
+                Console.WriteLine(i);
             }
-
-
-            Console.WriteLine($"{sayi}");
         }
     }
-}
+}          
